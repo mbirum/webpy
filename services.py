9f581fb9f3abbe09258ef83af2f071ff394d3c33
Matt Birum <45514707+mbirum@users.noreply.github.com>
2020-08-01 16:01:48 -0400

    Shell

--- services.py
+++ services.py
@@ -1,8 +1,4 @@
-import RPi.GPIO as GPIO
 import web
-from lib.tree import turnTreeOn
-from lib.tree import turnTreeOff
-from lib.tree import getTree
 
 urls = (
     '/', 'index',
@@ -17,16 +13,14 @@ class index:
 
 class gettree:
      def GET(self):
-          return getTree()
+          return "Get tree"
 
 class treeon:
      def POST(self):
-          turnTreeOn()
-          return "Tree on."
+          return "Tree on"
 
 class treeoff:
      def POST(self):
-          turnTreeOff()
           return "Tree off."
 
 if __name__ == "__main__":
